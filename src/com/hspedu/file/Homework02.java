9f45dc670bc518830ad3c431c44bbe7c49ec7855
EricMike <105481714+EricMike@users.noreply.github.com>
2022-09-21 18:13:40 +0800

    "snapshot01"

---
+++ src/com/hspedu/file/Homework02.java
@@ -0,0 +1,14 @@
+package com.hspedu.file;
+
+public class Homework02 {
+    public static void main(String[] args) {
+        String content = "-0.189";
+        String regStr = "^[-+]?([1-9]\\d*|0)(\\.\\d+)?$";
+
+        if (content.matches(regStr)){
+            System.out.println("匹配成功 是整数或者小数");
+        }else {
+            System.out.println("匹配失败");
+        }
+    }
+}
